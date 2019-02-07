671225ef35823c82a1ae5984653b4c35b3ee6c1f
Irum <irum@Irums-MacBook-Air.local>
2019-02-07 18:37:01 +0000

    @_@ homepage

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,22 +5,27 @@ import image from "../images/face-press-banner.jpg";
 
 const Header = () => {
   return (
-    <div className="ui secondary pointing menu">
-      <Link to="/" className="item">
-        Book
-      </Link>
-      <img src={image} />
-      <hr />
+    <>
       <div className="center aligned">
-        <h1>@_@</h1>
-      </div>
-      <div className="right menu">
         <Link to="/" className="item">
-          All Books
+          <h1>@_@</h1>
         </Link>
-        <GoogleAuth />
       </div>
-    </div>
+      <div className="ui secondary pointing menu">
+        <Link to="/writers" className="item">
+          All Writers
+        </Link>
+        <img src={image} alt="almost" />
+        <hr />
+
+        <div className="right menu">
+          <Link to="/books" className="item">
+            All Books
+          </Link>
+          <GoogleAuth />
+        </div>
+      </div>
+    </>
   );
 };
 export default Header;
